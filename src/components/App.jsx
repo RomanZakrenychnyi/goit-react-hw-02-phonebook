896ba72c5c543d1b1308f09a9735e3adf3cbbb09
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-28 00:03:17 +0300

    step 2 is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -5,17 +6,22 @@ import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
   };
 
   render() {
     return (
       <div>
-        <Section title='Phonebook'>
+        <Section title="Phonebook">
           <ContactForm />
         </Section>
-        <Section title='Contacts'>
+        <Section title="Contacts">
           <ContactList />
         </Section>
       </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,28 @@
-import style from './ContactForm.module.css'
+import style from './ContactForm.module.css';
 
 export const ContactForm = () => {
   return (
     <form className={style.form}>
-      <label>
-        <p>Name</p>
-        <input className={style.input}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
-      <button className={style.btn} type="submit">Add contact</button>
+      <label htmlFor="text">Name</label>
+      <input
+        className={style.input}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label htmlFor="text">Number</label>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button className={style.btn} type="submit">
+        Add contact
+      </button>
     </form>
   );
 };
