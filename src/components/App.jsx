4b0e09e003be83c6219a814a264fe8ebe00e6a5c
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-28 14:17:51 +0300

    create filter markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+
 
 export class App extends Component {
   state = {
@@ -12,7 +14,9 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    filter: '',
     name: '',
+    number: '',
   };
 
   handleDeleteBtnClick = id => {
@@ -31,6 +35,7 @@ export class App extends Component {
         <Section title="Phonebook">
           <ContactForm />
         </Section>
+        <Filter/>
         <Section title="Contacts">
           <ContactList
             contacts={contacts}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import style from './Filter.module.css';
+
+export const Filter = () => {
+  return (
+    <>
+      <form className={style.form}>
+        <label className={style.label}>Find contact by name</label>
+        <input
+        className={style.input}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      </form>
+      
+    </>
+  );
+};
